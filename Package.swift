7a7440b03861a0530d2b0f0e0c4ef4683f42edad
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:31:48 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "RegexRouteMatcher",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 5),
     ]
 )
