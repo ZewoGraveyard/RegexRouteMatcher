eb95cc8f461d47b352489366cce5c4c59438be45
Dan Appel <dan.appel00@gmail.com>
2016-06-08 00:12:25 -0700

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "RegexRouteMatcher",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 6),
+        .Package(url: "https://github.com/Zewo/POSIXRegex.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/Zewo/PathParameterMiddleware.git", majorVersion: 0, minor: 7),
     ]
 )
